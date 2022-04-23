7d993c3c1e1f152fe14efcda5c3a4ad8697048fe
Di Mann <di@delta9.ooo>
2022-04-23 15:42:36 -0400

    -Forge: Now able to Edit active profile. -Forge: Linked edit profile urls to javascript -Forge: Modified templates for forms to support profile editing -Dashboard: Added Forge entry for profile editing -Community: implemented 'restricted' flag.

--- forge/user_urls.py
+++ forge/user_urls.py
@@ -2,5 +2,7 @@ from django.urls import path,include
 from forge import user_views
 urlpatterns = [
     path('create/profile/',user_views.create_profile),
-    path('create/profile/preview/',user_views.create_profile_preview)    
+    path('create/profile/preview/',user_views.create_profile_preview),
+    path('edit/profile/',user_views.edit_profile),
+    path('edit/profile/preview/',user_views.edit_profile_preview)
 ]
--- forge/user_views.py
+++ forge/user_views.py
@@ -21,6 +21,23 @@ def create_profile(request):
         return render(request,"forge-create_profile.html",context)
 
 
+# Render the Edit Profile Forge Page:
+@login_required
+def edit_profile(request):
+    #  Ignore port:
+    vhost = request.META["HTTP_HOST"].split(":")[0];
+    community = (vhosts.get_vhost_community(hostname=vhost))
+    # The FORGE will create a new profile using this view. The first step is to get a placeholder profile so we can start assigning items and data to it:
+    profile = profiles.get_active_profile(request)
+    if community is None:
+        return render(request,"error-no_vhost_configured.html",{})
+    else:
+        request.session['community'] = str(community.uuid)
+        context = {'community':community,'vhost':vhost,'profile':profile,"av_path":settings.PLY_AVATAR_FILE_URL_BASE_URL}
+        return render(request,"forge-create_profile.html",context)
+
+
+
 
 # Render the Create Profile Preview Forge Page:
 @login_required
@@ -30,6 +47,17 @@ def create_profile_preview(request):
     community = (vhosts.get_vhost_community(hostname=vhost))
     # The FORGE will create a new profile using this view. The first step is to get a placeholder profile so we can start assigning items and data to it:
     profile = profiles.get_placeholder_profile(request)
+    context = {'community':community,'vhost':vhost,'profile':profile,"av_path":settings.PLY_AVATAR_FILE_URL_BASE_URL}
+    return render(request,"forge-preview_profile.html",context)
+
+# Render the Edit Profile Preview Forge Page:
+@login_required
+def edit_profile_preview(request):
+    #  Ignore port:
+    vhost = request.META["HTTP_HOST"].split(":")[0];
+    community = (vhosts.get_vhost_community(hostname=vhost))
+    # The FORGE will create a new profile using this view. The first step is to get a placeholder profile so we can start assigning items and data to it:
+    profile = profiles.get_active_profile(request)
     context = {'community':community,'vhost':vhost,'profile':profile,"av_path":settings.PLY_AVATAR_FILE_URL_BASE_URL}
     return render(request,"forge-preview_profile.html",context)
 
