ba4d52a77a68db4babc0b1b1a98a0e86f620ec4d
Di Mann <di@delta9.ooo>
2022-04-26 13:42:31 +0000

    -Cleaned up Migrations from Django 3.0.2 to Django 4.0 to enable installation in new nodes. -Almanac forms race condition when database not installed fixed.

--- almanac/forms.py
+++ almanac/forms.py
@@ -2,10 +2,16 @@ from django import forms
 from martor.fields import MartorFormField
 from dynapages.models import Templates
 class NewPageForm(forms.Form):
-    templates = Templates.objects.filter(archived=False,blocked=False)
     dynaPages = []
-    for tem in templates:
-        dynaPages.append((tem.template_id,tem.label))
+    try:
+        templates = Templates.objects.filter(archived=False,blocked=False)
+        for tem in templates:
+            dynaPages.append((tem.template_id,tem.label))
+    # Workaround to allow installation on blank databases:
+    except:
+        templates = []
+        
+
     title = forms.CharField(label='Page Title:')
     introduction = forms.CharField(label='Short Introduction/TL;DR:')
     dynaPage = forms.ChoiceField(label='DynaPage Template:',choices=dynaPages)
