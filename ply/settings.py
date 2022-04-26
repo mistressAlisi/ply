22a5fcf16d95a9199d6e472f002fd49b428480b2
Di Mann <di@delta9.ooo>
2022-04-26 17:40:52 -0400

    Added debug flag

--- ply/settings.py
+++ ply/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY") 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('PLY_DEBUG') == 'TRUE'
 
 #TODO: MAKE IT SO ALLOWED HOSTS ARE OPTIONAL!
 ALLOWED_HOSTS = [config("ALLOWED_HOST_1"),config("ALLOWED_HOST_2"),config("ALLOWED_HOST_3")] 
