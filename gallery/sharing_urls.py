451af071128d313bd4f5c7d95216b0bf11c6a763
Di Mann <di@delta9.ooo>
2022-05-12 16:59:48 -0400

    Modifing share url

--- gallery/sharing_urls.py
+++ gallery/sharing_urls.py
@@ -2,6 +2,6 @@ from django.urls import path,include
 from gallery import sharing_views
 import ply
 urlpatterns = [
-    path('@<slug:profile_id>/<uuid:collection_id>/<uuid:item_id>',sharing_views.gallery_item),
+    path('<slug:profile_id>/<uuid:collection_id>/<uuid:item_id>',sharing_views.gallery_item),
 
 ]
