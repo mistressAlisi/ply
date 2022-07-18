05ffe4f203ae77e2b011139363cbf2febc639688
LakesideMiners <23389169+LakesideMiners@users.noreply.github.com>
2022-07-17 21:11:42 -0400

    over 5 hours

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,79 @@
-# this file is left blank so I can just get a new branch :3
+# base image
+FROM python:3.10-slim-bullseye
+
+#maintainer
+LABEL DockerfileAuthor="LakesideMiners"
+
+# The enviroment variable ensures that the python output is set straight
+# to the terminal with out buffering it first
+ENV PYTHONBUFFERED 1
+
+
+RUN apt-get update -y \
+    && apt-get install libpq-dev pwgen gcc -y
+
+RUN ls
+#directory to store app source code
+RUN mkdir /app
+
+RUN touch "UWU"
+
+RUN ls 
+#switch to /app directory so that everything runs from here
+WORKDIR /app
+RUN touch "OwO"
+
+RUN ls
+#copy the app code to image working directory
+COPY ./ /app
+
+RUN ls /app
+#let pip install required packages
+RUN pip --root-user-action=ignore install -r requirements.txt
+
+# not so long ago, in a dockerfile not so far away.
+RUN echo "**** Starting Database Setup...****"
+
+RUN python3 ./manage.py migrate
+
+RUN echo "**** Installing SQL Files...****"
+
+RUN python3 ./manage.py install_sql_files _all_
+
+RUN echo "**** --> CREATING SUPERUSER / ADMINISTRATOR <--****"
+
+RUN python3 ./manage.py createsuperuser
+
+RUN echo "**** Initalizing Timezone Data...****"
+
+RUN python3 ./manage.py init_timezones
+
+RUN echo "**** Dynapages: Register Templates...****"
+
+RUN python3 ./manage.py registertemplate
+
+RUN echo "**** Dynapages: Register Widgets...****"
+
+RUN python3 ./manage.py registerwidgets
+
+RUN echo "**** Dynapages: Init Dynapages...****"
+
+RUN python3 ./manage.py init_dynapages
+
+RUN echo "*** Loading Disciplines and Categories (defaults): ***"
+
+RUN python3 ./manage.py load_disciplines DOCUMENTATION/Categories/disciplines.tsv
+
+RUN python3 ./manage.py load_categories DOCUMENTATION/Categories/categories.tsv
+
+RUN echo "*** Creating Initial Community ***"
+
+RUN python3 ./manage.py create_community
+
+RUN echo "****** SETUP COMPLETE! Further parameters can be modified in 'config/settings.ini'! *****"
+
+RUN echo "You may (now hopefully) start the Django environment using runserver! :3!"
+
+RUN echo "Thank you for using Ply! :3 :3"
+
+RUN python3 manage.py runserver 0.0.0.0:8000 
--- setup/setup.sh
+++ setup/setup.sh
@@ -17,46 +17,43 @@ echo "***Configuration will be generated as 'config/settings.ini': Please mkdir 
 read -p "***WARNING: Proceeding past this point WILL replace existing configuration settings (and secret keys). Ctrl-C now if you wish to abort, or press enter to continue...***" abort
 read -n 1 -p "Do you wish to install PostgreSQL Locally? (y/n): " ipq
 echo "\n"
-if [ $ipq == 'y' ];
-then
+if [ $ipq == 'y' ]; then
     echo "Installing PostgreSQL..."
     sudo apt-get install postgresql-13
     echo "Installation Complete: PSQL Must be configured!"
     read -p "*** Please configure 'pg_hba.conf' and 'postgresql.conf' and press Enter to continue when ready. ***" abort
 fi
-    read -p "*** To continue installation; you will require a PostgreSQL user and database set up. The user should be the owner of the database. Please create both objects and press enter to continue when ready. ***" abort
+read -p "*** To continue installation; you will require a PostgreSQL user and database set up. The user should be the owner of the database. Please create both objects and press enter to continue when ready. ***" abort
 echo "***** Please enter the PostgreSQL Configuration parameters below: *****"
 conf_valid=false
-while [ $conf_valid == false ]
-do
-DB_HOST='127.0.0.1'
-read  -p "Host name / IP? [127.0.0.1]: " nDB_HOST
-DB_HOST=${nDB_HOST:-$DB_HOST}
+while [ $conf_valid == false ]; do
+    DB_HOST='127.0.0.1'
+    read -p "Host name / IP? [127.0.0.1]: " nDB_HOST
+    DB_HOST=${nDB_HOST:-$DB_HOST}
 
-DB_PORT='5432'
-read  -p "Port? [5432]: " nDB_PORT
-DB_PORT=${nDB_PORT:-$DB_PORT}
+    DB_PORT='5432'
+    read -p "Port? [5432]: " nDB_PORT
+    DB_PORT=${nDB_PORT:-$DB_PORT}
 
-DB_NAME='ply'
-read  -p "DB name? [ply]: " nDB_NAME
-DB_NAME=${nDB_NAME:-$DB_NAME}
+    DB_NAME='ply'
+    read -p "DB name? [ply]: " nDB_NAME
+    DB_NAME=${nDB_NAME:-$DB_NAME}
 
-DB_USER='ply'
-read  -p "User name? [ply]: " nDB_USER
-DB_USER=${nDB_USER:-$DB_USER}
+    DB_USER='ply'
+    read -p "User name? [ply]: " nDB_USER
+    DB_USER=${nDB_USER:-$DB_USER}
 
-DB_PW='ply'
-read -s -p "User Password? (will not be echoed) [ply]: " nDB_PW
-echo "\n"
-DB_PW=${nDB_PW:-$DB_PW}
-echo "*** Connection String is: pgsql://$DB_USER@$DB_HOST:$DB_PORT/$DB_NAME ***"
-read -n1 -p "Is this correct? (y/n): " correct
-if [ $correct == 'y' ];
-then
-    conf_valid=true
-fi
-echo "\n"
-done;
+    DB_PW='ply'
+    read -s -p "User Password? (will not be echoed) [ply]: " nDB_PW
+    echo "\n"
+    DB_PW=${nDB_PW:-$DB_PW}
+    echo "*** Connection String is: pgsql://$DB_USER@$DB_HOST:$DB_PORT/$DB_NAME ***"
+    read -n1 -p "Is this correct? (y/n): " correct
+    if [ $correct == 'y' ]; then
+        conf_valid=true
+    fi
+    echo "\n"
+done
 echo "Generating Config file 'config/settings.ini'..."
 cp setup/INI_DEF config/settings.ini
 sed -i "s/DBNAME/${DB_NAME}/" config/settings.ini
@@ -64,41 +61,31 @@ sed -i "s/DBUSER/${DB_USER}/" config/settings.ini
 sed -i "s/DBPW/${DB_PW}/" config/settings.ini
 sed -i "s/DBHOST/${DB_HOST}/" config/settings.ini
 sed -i "s/DBPORT/${DB_PORT}/" config/settings.ini
-SECRETS=`pwgen -s 230 1`
+SECRETS=$(pwgen -s 230 1)
 sed -i "s/SKEY/${SECRETS}/" config/settings.ini
 
 echo "**** Starting Database Setup...****"
 python3 ./manage.py migrate
-
 echo "**** Installing SQL Files...****"
 python3 ./manage.py install_sql_files _all_
-
 echo "**** --> CREATING SUPERUSER / ADMINISTRATOR <--****"
 python3 ./manage.py createsuperuser
-
 echo "**** Initalizing Timezone Data...****"
 python3 ./manage.py init_timezones
-
 echo "**** Dynapages: Register Templates...****"
 python3 ./manage.py registertemplate
-
 echo "**** Dynapages: Register Widgets...****"
 python3 ./manage.py registerwidgets
 
 echo "**** Dynapages: Init Dynapages...****"
-python3 ./manage.py init_dynapages
 
+python3 ./manage.py init_dynapages
 echo "*** Loading Disciplines and Categories (defaults): ***"
 python3 ./manage.py load_disciplines DOCUMENTATION/Categories/disciplines.tsv
 python3 ./manage.py load_categories DOCUMENTATION/Categories/categories.tsv
-
-
 echo "*** Creating Initial Community ***"
 python3 ./manage.py create_community
-
-
 echo "****** SETUP COMPLETE! Further parameters can be modified in 'config/settings.ini'! *****"
 echo "You may (now hopefully) start the Django environment using runserver! :3!"
 echo "Thank you for using Ply! :3 :3"
-
-
+python3 manage.py runserver 0.0.0.0:8000 
