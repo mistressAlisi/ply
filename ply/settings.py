e611fd646addbe32c7f23c47f1d028811a9a0020
Matt Sowden <msowden@consultaegis.com>
2024-02-01 18:58:29 -0500

    Add sentry

--- ply/settings.py
+++ ply/settings.py
@@ -393,3 +393,16 @@ else:
     PAYMENT_STRIPE_DONATION_ITEM = config('UFLS_PAYMENT_DONATION_ITEM', default=False)
 
 PLY_DEFAULT_THEME = config('PLY_DEFAULT_THEME',default="core.ui.themes.default")
+
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://0afd7530613dabd4db8e14135dfbc3bc@o334180.ingest.sentry.io/4506674252283904",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
