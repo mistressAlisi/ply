4346413d0c7d55080be246f6f6f8d40160d2b96e
Matt Sowden <msowden@consultaegis.com>
2024-02-02 14:07:04 -0500

    remove duplicates

--- ply/settings.py
+++ ply/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'django_bootstrap5',
-    'jsignature',
     'django_registration',
     'storages',
     'martor',
