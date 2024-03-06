1ab1f2aaace53d428c13d70ee3c7ded3d7f970f2
Di Mann <di@delta9.ooo>
2024-03-06 12:46:22 -0500

    -Ply: -Enhancements to step2.sh in the setup.

--- setup/step2.sh
+++ setup/step2.sh
@@ -1,4 +1,10 @@
-
+echo "**** Assuming the Virtual Environment was created in step1.sh! If not, ctrl-c now!...****"
+read -p "Enter the path that the virtual environment is in, or enter for default: [~/venv/ply]: " npath
+path=${npath:-$path}
+fpath="$path/ply"
+echo "Creating and activating venv: $fpath"
+python3 -m venv "$fpath"
+source $fpath'/bin/activate'
 echo "**** Starting Database Setup...****"
 python3 ./manage.py migrate
 
