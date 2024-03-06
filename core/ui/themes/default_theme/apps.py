35c9ae09c4427dac0c5387d2ce70f006e4cf7fe5
Di Mann <di@delta9.ooo>
2024-03-06 16:26:09 -0500

    -Ply: -Default theme getting fixed.

--- core/ui/themes/default_theme/apps.py
+++ core/ui/themes/default_theme/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DefaultConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core.ui.themes.default'
+    name = 'core.ui.themes.default_theme'
