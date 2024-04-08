ed53354c4e61cfcfa7ff8ab2abf3d4763827ee77
Matt Sowden <msowden@consultaegis.com>
2024-04-08 12:14:53 -0400

    Add corsheaders middleware

--- ply/settings.py
+++ ply/settings.py
@@ -111,6 +111,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
