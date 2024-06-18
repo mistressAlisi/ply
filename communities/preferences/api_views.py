86ec6dda4b1979ea2db7ee023777ab12e9d83e86
Di Mann <di@delta9.ooo>
2024-06-18 19:37:34 -0400

    -Ply: -Preferences Application ported to new Javascript Dashboard API (AbstractDashboardApp) -ply_appinfo added for the above.

--- communities/preferences/api_views.py
+++ communities/preferences/api_views.py
@@ -9,8 +9,8 @@ from communities.preferences.forms import PreferencesForm
 @transaction.atomic
 def save_system_settings(request):
     uprefs = Preferences.objects.get_or_create(user=request.user)[0]
-    form_saver = PreferencesForm(request.POST,instance=prefs)
+    form_saver = PreferencesForm(request.POST,instance=uprefs)
     if (not form_saver.is_valid()):
         return JsonResponse({"res":"err","e":str(form_saver.errors.as_data())},safe=False)
     form_saver.save()
-    return JsonResponse("ok",safe=False)
+    return JsonResponse({"res":"ok"},safe=False)
