a40489fd9ae19a778b08bd74bbea2e2fc33f9539
Di Mann <di@delta9.ooo>
2024-07-06 23:43:12 -0400

    -PLY: -Major release bump: New Version 2024.242.030:a2: -Introduces new AbstractDashboardApp functions and classes. -Introduces new simple_serialiser. -Introduces new Stream APIs for named streams. -Introduces stream toolkit for named posting. -Enhancements to WidgetFactory. -Dashboard applications can now be patterned using the above APIs quickly..

--- dashboard/static/dashboard/js/Dashboard/AbstractDashboardApp.js
+++ dashboard/static/dashboard/js/Dashboard/AbstractDashboardApp.js
@@ -3,13 +3,17 @@ export class AbstractDashboardApp {
     _appName = "app"
     settings = {
         "offcanvas":false,
+        "offcanvas_body":"#ofccontents",
         "modal":"#some_modal",
-        "form":"#some_form"
+        "form":"#some_form",
+        "home_on_success":true,
+        "success_hdr":"Success!",
+        "success_body":"Operation Complete!"
     }
     urls = {
-            "submit":"/some/url"
+            "submit":"/some/url",
+            "load_edit":"/some/url/"
     }
-
     modal = false
     offcanvas = false
     widget_factory =  false
@@ -27,8 +31,8 @@ export class AbstractDashboardApp {
     _submitHandle(data,stat,home=true) {
             //console.log(data)
             if (data.responseJSON.res == "ok") {
-                dashboard.successToast('<h6><i class="fa-solid fa-check"></i>&#160;Success!','Operation complete!');
-                if (home == true) {
+                dashboard.successToast('<h6><i class="fa-solid fa-check"></i>&#160;'+this.settings["success_hdr"],this.settings["success_body"]);
+                if ((home == true) && (this.settings["home_on_success"] == true)) {
                     dashboard.panel_home();
                 } else {
                     dashboard.dc_reloadPanel();
@@ -36,8 +40,14 @@ export class AbstractDashboardApp {
                 return true;
 
             } else {
-                dashboard.errorToast('<h6><i class="fa-solid fa-xmark"></i>&#160;Error!','An Error Occured! '+data.responseJSON.e.__all__[0]);
-                console.error("Unable to execute Operation: ",data.responseJSON.e)
+                var errStr="";
+                var objks = Object.keys(data.responseJSON.e);
+                for (var okin in objks) {
+                    var ok = objks[okin];
+                    errStr = errStr +" <em>"+ok+"</em>: "+data.responseJSON.e[ok]+"<br/>";
+                }
+                dashboard.errorToast('<h5 class="text-danger"><i class="fa-solid fa-xmark"></i>&#160;Error!','<strong>An Error Occurred:</strong><br/>'+errStr);
+                console.error("Unable to execute Operation: ",errStr)
                 return false;
             }
     }
@@ -47,12 +57,11 @@ export class AbstractDashboardApp {
     }
 
     _showModal() {
-        if (this.modal == false) {
-            this._getModal();
-        }
+        this._getModal();
         this.modal.show();
     }
     _hideModal() {
+        this._getModal();
         this.modal.hide();
     }
 
@@ -69,7 +78,7 @@ export class AbstractDashboardApp {
             if (handle == false) {
                 handle = this._submitHandle;
             }
-            console.log("Submitting Dashboard App Data: URL '"+url+"'");
+            console.info("Submitting Dashboard App Data: URL '"+url+"'");
             $.ajax({
             url:  url,
             data:     form.serialize(),
@@ -79,6 +88,32 @@ export class AbstractDashboardApp {
             })
 
 
+    }
+    _edit_handle(data) {
+        if (data.res == "ok") {
+            var data_keys = Object.keys(data.data);
+            for (var i in data_keys) {
+                var dk = data_keys[i];
+                var update_el = $(this.elements["form"]).find("#id_"+dk);
+                if (update_el.length > 0) {
+                    update_el[0].value = data.data[dk];
+                }
+            }
+            this._showModal();
+        } else {
+                dashboard.errorToast('<h6><i class="fa-solid fa-xmark"></i>&#160;Error!','An Error Occured! '+data.e.__all__[0]);
+                console.error("Unable to execute Operation: ",data.e)
+                return false;
+            }
+    }
+    edit(uuid) {
+          console.info("Loading Data for Edit Mode for '"+uuid+"'");
+            $.ajax({
+            url:   this.urls["load_edit"]+uuid,
+            success: this._edit_handle,
+            method: 'GET',
+            context: this
+            })
     }
     _getOffcanvas() {
             this.offcanvas = new bootstrap.Offcanvas(this.elements["offcanvas"]);
@@ -98,6 +133,50 @@ export class AbstractDashboardApp {
     _toggleOffcanvas() {
         this.offcanvas.toggle();
     }
+
+    _offcanvas_details(data) {
+        if (data.res == "ok") {
+            this._getOffcanvas();
+            var container = $(this.offcanvas._element).find(this.settings["offcanvas_body"]);
+            container.empty();
+            var table = this.widget_factory.create_table(data.data.uuid);
+            container.append(table);
+            var data_keys = Object.keys(data.data);
+            for (var i in data_keys) {
+                var dk = data_keys[i];
+                var tr = this.widget_factory.create_tr(data.data.uuid);
+                table.append(tr);
+                var lbl = dk;
+                if (data.verbose_names[dk] != "") {
+                    lbl = data.verbose_names[dk];
+                }
+                var th = this.widget_factory.create_th(data.data.uuid,dk+"th",lbl+":");
+                tr.append(th);
+                var txt = "&#160;";
+                if (data.data[dk] != null) {
+                    txt = data.data[dk];
+                }
+                var td = this.widget_factory.create_td(data.data.uuid, dk + "td", txt);
+                var tr2 = this.widget_factory.create_tr(data.data.uuid);
+
+                table.append(tr2);
+                tr2.append(td);
+            }
+
+            this._showOffcanvas();
+        }
+    }
+
+    offcanvas_details(uuid) {
+          console.info("Loading Data for Offcanvas Details for '"+uuid+"'");
+            $.ajax({
+            url:   this.urls["load_edit"]+uuid,
+            success: this._offcanvas_details,
+            method: 'GET',
+            context: this
+            })
+    }
+
      constructor(name="app",settings={},urls={}) {
         if (name != "app") {
             this._appName = name;
@@ -108,8 +187,10 @@ export class AbstractDashboardApp {
         this.widget_factory = new WidgetFactory();
         this.elements = {
             "form":this.settings["form"],
-            "modal":this.settings["modal"]
+            "modal":this.settings["modal"],
+            "offcanvas":this.settings["offcanvas"]
         }
+
         console.info("Dashboard App ",this._appName," initialised!");
      }
 }
