579c0d20a26489a94ba79ca75b5c819ee07b7d1a
Matt Sowden <msowden@consultaegis.com>
2024-12-19 14:28:25 -0500

    addtl stuff

--- ply/settings.py
+++ ply/settings.py
@@ -31,6 +31,8 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('PLY_DEBUG') == 'TRUE'
 
+IN_DEVELOPER_MODE = config('MIDSUMMER_IN_DEVELOPER_MODE') == 'TRUE'
+
 #TODO: MAKE IT SO ALLOWED HOSTS ARE OPTIONAL!
 #ALLOWED_HOSTS = [config("ALLOWED_HOST_1"),config("ALLOWED_HOST_2"),config("ALLOWED_HOST_3")] 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
