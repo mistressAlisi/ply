fbd6513924f674c935a59a278591da6546d09df2
Matthew Sowden <msowden@consultaegis.com>
2025-03-04 18:41:31 -0500

    ufls

--- manage.py
+++ manage.py
@@ -7,6 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ply.settings')
+    os.environ.setdefault('MIDSUMMER_RUNNING_MANAGE', 'true')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
